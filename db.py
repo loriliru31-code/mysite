c80c00d86791ec38b708966fa2957e26f8778d5d
loroliru <loriliru31@gmail.com>
2026-04-06 21:36:49 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,3 +1,22 @@
-from flask_mysqldb import MySQL
+import pymysql
+from flask import g
+from config import *
 
-mysql = MySQL()
+# Функция для получения подключения к базе
+def get_db():
+    if "db" not in g:
+        g.db = pymysql.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DB,
+            charset="utf8mb4",
+            cursorclass=pymysql.cursors.DictCursor  # возвращает словари вместо кортежей
+        )
+    return g.db
+
+# Функция для закрытия соединения после запроса
+def close_db(e=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
